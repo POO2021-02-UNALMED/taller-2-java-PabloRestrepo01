e174eb7600ded48d1707efd3a9a9743bb8c48aa8
PabloRestrepo01 <70071301+PabloRestrepo01@users.noreply.github.com>
2021-10-15 16:06:43 -0500

    Clases

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,34 @@
+package test;
+public class Auto {
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		int cantidad = 0;
+		
+		for(int i = 0; i < asientos.length; i++) {
+			if(asientos[i] != null) {
+				cantidad++;
+			}
+		}
+		
+		return cantidad;
+	}
+	
+	String verficarIntegtidad() {
+		if(motor.registro != registro)
+			return "Las piezas no son originales";
+		
+		for(int i = 0; i < asientos.length; i++) {
+			if(asientos[i].registro != registro) {
+				return "Las piezas no son originales";
+			}
+		}
+		return "Auto original";
+	}
+}
